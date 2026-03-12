5964d6ec5f6569455cba545ba1973dc11b29e46f
eesaBase <eesa@chatbase.co>
2026-03-12 04:45:41 +0200

    Initial Commit

---
+++ tatbeeqMa7mool/tatbeeqMa7moolApp.swift
@@ -0,0 +1,17 @@
+//
+//  tatbeeqMa7moolApp.swift
+//  tatbeeqMa7mool
+//
+//  Created by Eesabase on 12/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct tatbeeqMa7moolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
